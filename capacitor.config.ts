70146909d7ccb717a700ec8826bf9bd364514722
Gautammer <mergautam9@gmail.com>
2022-05-17 11:20:29 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'twitter_ui',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
